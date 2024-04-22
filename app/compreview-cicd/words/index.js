e84e2e6b62324799d4288555872f7e29393214ec
Salifcool <43956221+Salifcool@users.noreply.github.com>
2024-04-22 12:39:46 +0000

    Update index.js

--- app/compreview-cicd/words/index.js
+++ app/compreview-cicd/words/index.js
@@ -1,5 +1,5 @@
 const Fastify = require("fastify");
-const { isPalindrome } = require("./palindrome");UYYYY
+const { isPalindrome } = require("./palindrome");UYYYYdf
 
 
 const api = Fastify({ logger: true });
